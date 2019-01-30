4331d0432efa96351fbca3acd65892414199d19e
Deepank Agrawal <deepank.361998@gmail.com>
2019-01-31 01:14:36 +0530

    [WIP]Update utility.jl
    Changed function protoype.
    Updating the documentation.

--- src/utility.jl
+++ src/utility.jl
@@ -1,16 +1,23 @@
 """
 ```
-visualize_optical_flow_hsv(flow)
+visualize_flow(ColorBased(), flow)
 ```
 
-Return the visualization of an optical flow vector `flow` in hsv color space.
+Returns an image that matches the dimensions of the input matrix and that depicts the 
+orientation and magnitude of the optical flow vectors using the HSV color space.
 
-Here, hue encodes angle between optical flow vector and the x-axis in the  image plane,
+# Details
+Hue encodes angle between optical flow vector and the x-axis in the image plane;
 saturation encodes the ratio between the individual vector magnitudes and the maximum magnitude 
-among the whole motion field.
+among the whole motion field, and the values always equal one.
+
+# Arguments
+The flow parameter needs to be a two-dimensional arrays of length-2 vectors (of type SVector) 
+which represent the displacement of each pixel.
+
 
 """
-function visualize_optical_flow_hsv(flow::Array{SVector{2, Float64}, 2})
+function visualize_flow(method::ColorBased, flow::Array{SVector{2, Float64}, 2})
  
     # Convert from (row,column) to (x,y) convention.
     map!(x-> SVector(last(x),first(x)), flow, flow)
